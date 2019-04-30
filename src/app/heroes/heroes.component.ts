8e7a3383a3e4f78120682048ed401a4fca16e261
Meville Pereira <meville.pereira@ge.com>
2019-04-30 15:48:58 -0500

    bug fix

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,27 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroService } from '../hero.service';
-
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+ 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
+ 
 export class HeroesComponent implements OnInit {
-
-  heroes: Hero[];
-
-
-  constructor(private heroService: HeroService) { }
-
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
-
+ 
+  heroes = HEROES;
+  selectedHero: Hero;
+ 
+  constructor() { }
+ 
   ngOnInit() {
-    this.getHeroes();
   }
-
+ 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
