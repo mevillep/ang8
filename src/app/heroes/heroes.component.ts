c9f55c5637c14f7ae1ad25d746973461f9fa2819
Meville Pereira <meville.pereira@ge.com>
2019-05-01 13:21:53 -0500

    add: service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
- 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,14 +10,21 @@ import { HEROES } from '../mock-heroes';
  
 export class HeroesComponent implements OnInit {
  
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
  
-  constructor() { }
- 
-  ngOnInit() {
+  constructor(private heroService: HeroService) { 
+    
   }
  
+  ngOnInit() {
+      this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
